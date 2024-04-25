52cfd601e7c7fa6dbf2078f6a3e23edef933f6fe
Rishi Ranajan <101rishidsr@gmail.com>
2024-04-25 16:42:26 +0530

    msg

---
+++ main.py
@@ -0,0 +1,79 @@
+from flask import Flask, request, render_template
+import numpy as np
+import pandas as pd
+import pickle
+
+app = Flask(__name__)
+
+# Load datasets
+sym_des = pd.read_csv("datasets/symtoms_df.csv")
+precautions = pd.read_csv("datasets/precautions_df.csv")
+workout = pd.read_csv("datasets/workout_df.csv")
+description = pd.read_csv("datasets/description.csv")
+medications = pd.read_csv('datasets/medications.csv')
+diets = pd.read_csv("datasets/diets.csv")
+
+# Load model
+svc = pickle.load(open('models/svc.pkl','rb'))
+
+# Helper function
+def helper(dis):
+    desc = description[description['Disease'] == dis]['Description'].iloc[0]
+    pre = precautions[precautions['Disease'] == dis][['Precaution_1', 'Precaution_2', 'Precaution_3', 'Precaution_4']].values.tolist()[0]
+    med = medications[medications['Disease'] == dis]['Medication'].values.tolist()
+    die = diets[diets['Disease'] == dis]['Diet'].values.tolist()
+    wrkout = workout[workout['disease'] == dis]['workout'].values.tolist()
+    return desc, pre, med, die, wrkout
+
+# Model Prediction function
+def get_predicted_value(patient_symptoms):
+    input_vector = np.zeros(len(symptoms_dict))
+    for item in patient_symptoms:
+        input_vector[symptoms_dict[item]] = 1
+    return diseases_list[svc.predict([input_vector])[0]]
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route('/predict', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        symptoms = request.form.get('symptoms')
+        if symptoms == "Symptoms":
+            message = "Please enter symptoms or you may have misspelled them."
+            return render_template('index.html', message=message)
+        else:
+            user_symptoms = [s.strip() for s in symptoms.split(',')]
+            user_symptoms = [symptom.strip("[]' ") for symptom in user_symptoms]
+            predicted_disease = get_predicted_value(user_symptoms)
+            dis_des, precautions, medications, rec_diet, workout = helper(predicted_disease)
+
+            my_precautions = []
+            for i in precautions:
+                my_precautions.append(i)
+
+            return render_template('index.html', predicted_disease=predicted_disease, dis_des=dis_des,
+                                   my_precautions=my_precautions, medications=medications, my_diet=rec_diet,
+                                   workout=workout)
+
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/developer')
+def developer():
+    return render_template("developer.html")
+
+@app.route('/blog')
+def blog():
+    return render_template("blog.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
